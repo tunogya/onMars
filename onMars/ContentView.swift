1db1a7d1a1c2bdc77550f43ec583252f0e093210
tunogya <tunogya@qq.com>
2021-03-28 15:55:32 +0800

    修复工程

--- onMars/ContentView.swift
+++ onMars/ContentView.swift
@@ -1,80 +1,48 @@
 //
 //  ContentView.swift
-//  onMars
+//  Shared
 //
-//  Created by 丁涯 on 2021/3/28.
+//  Created by 丁涯 on 2021/3/11.
 //
 
 import SwiftUI
-import CoreData
+import SpriteKit
+import CoreMotion
+
+let screen = UIScreen.main.bounds
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @ObservedObject var motion: MotionManager = MotionManager()
+    
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: screen.width, height: screen.height)
+        return scene
+    }
+    
+    var debug: some View  {
+        VStack {
+            Text("Magnetometer Data")
+            Text("X: \(motion.x)")
+            Text("Y: \(motion.y)")
+            Text("Z: \(motion.z)")
+        }
+    }
+    
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-            }
-            .onDelete(perform: deleteItems)
+        ZStack {
+            SpriteView(scene: scene)
+                .frame(width: screen.width, height: screen.height)
+                .ignoresSafeArea()
+            debug
         }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
